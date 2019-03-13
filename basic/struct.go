16ab1b67cad30b7a7cb0d27af596c31ecc107f08
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-13 20:40:09 +0900

    構造体の埋め込みを試してみた

--- basic/struct.go
+++ basic/struct.go
@@ -8,32 +8,53 @@ type ID int
 // Priority of Task
 type Priority int
 
+// User name
+type User struct {
+	FirstName string
+	LastName  string
+}
+
+// FullName return user full name
+func (u *User) FullName() string {
+	return fmt.Sprintf("%s %s", u.FirstName, u.LastName)
+}
+
+// NewUser return new user object
+func NewUser(firstName string, lastName string) *User {
+	return &User{
+		FirstName: firstName,
+		LastName:  lastName,
+	}
+}
+
 // Task Struct definition
 type Task struct {
 	id       ID
 	priority Priority
 	detail   string
 	done     bool
+	*User
 }
 
 // NewTask is a pseudo Constructor
-func NewTask(id ID, detail string) *Task {
+func NewTask(id ID, detail string, firstName string, lastName string) *Task {
 	task := &Task{
 		id:       id,
 		priority: 0,
 		detail:   detail,
 		done:     false,
+		User:     NewUser(firstName, lastName),
 	}
 	return task
 }
 
 // Receiver
 func (task Task) String() string {
-	str := fmt.Sprintf("%d) %s", task.id, task.detail)
+	str := fmt.Sprintf("(%d) %s -> %s", task.id, task.detail, task.FullName())
 	return str
 }
 
 func main() {
-	task := NewTask(1, "Sample Task")
+	task := NewTask(1, "Sample Task", "Yuichi", "Tsunematsu")
 	fmt.Println(task)
 }
